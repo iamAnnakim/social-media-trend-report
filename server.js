4e7f72922473f4026370deb4032698da324445df
pjs030226 <qkrwotjdhanl@gmail.com>
2025-06-10 15:08:28 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 // ✅ Google Trends - 더미 데이터 사용
 app.get('/api/google-trends', (req, res) => {
   const dummykeywords = [
-    '#이재명아들', '#사전투표기간', '#카리나', '#최정우', '#한화대lg', '#롯데대삼성', '#키움대kia', '#사전투표소'
+    '#김종석', '#메이플 렌', '#잡식공룡', '#월드컵 예선', '#wwdc', '#강유정', '#2026 6월 모의고사', '#g7'
   ];
   res.json({ keywords: dummykeywords });
 });
@@ -35,15 +35,15 @@ app.get('/api/X-trends', async (req, res) => {
 // ✅ YouTube Trending - 더미 데이터 사용
 app.get('/api/youtube-trending', (req, res) => {
   const dummyVideos = [
-    { title: '젓가락', views: '1250000', category: 'Lifestyle' },
-    { title: '사전투표', views: '890000', category: 'Politics' },
-    { title: '배인규', views: '1570000', category: 'Entertainment' },
-    { title: '투표', views: '2200000', category: 'Politics' },
-    { title: '나인 퍼즐', views: '730000', category: 'Lifestyle' },
-    { title: '이준석 젓가락', views: '730000', category: 'Politics' },
-    { title: '이재명 젓가락', views: '730000', category: 'Politics' },
-    { title: '대선 토론 3차', views: '730000', category: 'Politics' },
-    { title: '이재명 아들들', views: '730000', category: 'Politics' },
+    { title: '김종석', views: '1250000', category: 'Lifestyle' },
+    { title: '축구 국가대표팀', views: '1010000', category: 'Sports' },
+    { title: '취임식', views: '990000', category: 'Politics' },
+    { title: '선거방송', views: '878000', category: 'Politics' },
+    { title: '광장', views: '852000', category: 'Lifestyle' },
+    { title: '잡식동물', views: '730000', category: 'Lifestyle' },
+    { title: '현충일', views: '654000', category: 'Lifestyle' },
+    { title: '오킹', views: '588000', category: 'Lifestyle' },
+    { title: '김혜경', views: '576000', category: 'Politics' },
   ];
   res.json({ videos: dummyVideos });
 });
